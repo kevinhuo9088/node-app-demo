e255d1e263b7c30b111639bfa9e9fc162c0a2ffa
kevinhuo9088 <39965617+kevinhuo9088@users.noreply.github.com>
2018-09-09 13:06:49 -0700

    initial file
    adding new file

---
+++ server.js
@@ -0,0 +1,49 @@
+//server.js
+//npm init
+//installing dependency
+//npm install express body-parser mongoose --save
+//npm install express --save//
+//npm install mongoose --save
+//npm install body-parser --save
+//We’ll now import the above database configuration in server.js and connect to the database using mongoose.
+//Add the following code to the server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+	useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Notes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+// Require Notes routes
+require('./app/routes/note.routes.js')(app);
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
